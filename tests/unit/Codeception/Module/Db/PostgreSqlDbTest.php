0c4883d396ffc9159ea9f0f5d51f8ff30830ade1
Vincent Dauce <eXorus@users.noreply.github.com>
2020-04-07 18:07:12 +0200

    Update PostgreSqlDbTest.php

--- tests/unit/Codeception/Module/Db/PostgreSqlDbTest.php
+++ tests/unit/Codeception/Module/Db/PostgreSqlDbTest.php
@@ -21,10 +21,18 @@ class PostgreSqlDbTest extends TestsForDb
         if (!function_exists('pg_connect')) {
             $this->markTestSkipped();
         }
+        
+        if (getenv('APPVEYOR')) {
+            $password = 'Password12!';
+        }
+        elseif (getenv('PGPASSWORD')) {
+            $password = getenv('PGPASSWORD');
+        }
+        
         return [
             'dsn' => 'pgsql:host=localhost;dbname=codeception_test',
             'user' => 'postgres',
-            'password' => 'postgres',
+            'password' => $password ? $password : null,
             'dump' => 'tests/data/dumps/postgres.sql',
             'reconnect' => true,
             'cleanup' => true,
