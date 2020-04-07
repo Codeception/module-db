f4d19eb6ba3f606ee56d396ca4692d4f363dd33d
Vincent Dauce <eXorus@users.noreply.github.com>
2020-04-07 13:50:09 +0200

    Update PostgresTest.php

--- tests/unit/Codeception/Lib/Driver/PostgresTest.php
+++ tests/unit/Codeception/Lib/Driver/PostgresTest.php
@@ -12,7 +12,7 @@ class PostgresTest extends Unit
     protected static $config = [
         'dsn' => 'pgsql:host=localhost;dbname=codeception_test',
         'user' => 'postgres',
-        'password' => getenv('DB_POSTGRESQL_PASSWORD'),
+        'password' => null,
     ];
 
     protected static $sql;
